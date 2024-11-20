5a7fd96a3ff2531ba7d94de402e3decfb172aab0
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-11-19 19:43:19 -0500

    Move Global Highlight to Route Group

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
 use App\Http\Controllers\ExperienceController;
+use App\Http\Middleware\HighlightKeywords;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::middleware([HighlightKeywords::class])->group(function () {
+    Route::get('/', function () {
+        return view('index');
+    });
 
-Route::get('/about', function () {
-    return view('about');
+    Route::get('/about', function () {
+        return view('about');
+    });
+
+    Route::get('/history', [ExperienceController::class, 'showExperience']);
 });
 
 Route::get('/skills', function () {
     return view('skills');
-});
-
-Route::get('/history', [ExperienceController::class, 'showExperience']);
+})->withoutMiddleware([HighlightKeywords::class]);
